c6911f8c6319cdace5951ec29eb17924e895eaa2
njguld95038043 <t.tamura6060@gmail.com>
2022-03-29 00:57:10 +0000

    [Add]devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  get 'books/index'
-  get 'books/edit'
-  get 'books/show'
+  devise_for :users
+  root to: 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
