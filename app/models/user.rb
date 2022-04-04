c935687be396a3d126c80cccc547c01b92cbabfc
njguld95038043 <t.tamura6060@gmail.com>
2022-04-04 05:26:10 +0000

    [Add]notification

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          has_many :favorites, dependent: :destroy
+
+         has_many :active_notifications, class_name: "Notification", foreign_key: "visiter_id", dependent: :destroy
+         has_many :passive_notifications, class_name: "Notification", foreign_key: "visited_id", dependent: :destroy
 end
