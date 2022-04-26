c05b6d86e1332fb779d2d64d6028c5a3e3ee70d0
njguld95038043 <t.tamura6060@gmail.com>
2022-04-26 06:09:37 +0000

    [Update]book.rb

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,20 @@
 class Book < ApplicationRecord
   belongs_to = user
+
+  def self.search(search, word)
+  return all if word.blank?
+
+  if search == "perfect_match"
+    where("nick_name = ?", "#{word}")
+  elsif search == "forward_match"
+    where("nick_name LIKE?", "#{word}%")
+  elsif search == "backward_match"
+    where("nick_name LIKE?", "%#{word}")
+  elsif search == "partial_match"
+    where("nick_name LIKE?", "%#{word}%")
+  else
+    all
+  end
+end
+
 end
