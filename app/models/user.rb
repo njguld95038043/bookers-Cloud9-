9f3eb470e857487c9ecd9631ee7e2f49dab522af
njguld95038043 <t.tamura6060@gmail.com>
2022-04-27 01:51:56 +0000

    [Add]ゲストログイン

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,19 @@ class User < ApplicationRecord
 
          has_many :active_notifications, class_name: "Notification", foreign_key: "visiter_id", dependent: :destroy
          has_many :passive_notifications, class_name: "Notification", foreign_key: "visited_id", dependent: :destroy
+
+  def self.guest
+    find_or_create_by!(name: 'guestuser' ,email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = "guestuser"
+    end
+  end
+
+  def self.guest
+    find_or_create_by!(name: 'guestuser' ,email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = "guestuser"
+    end
+  end
+
 end
